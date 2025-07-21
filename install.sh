f3e50f019a0ac08ca7edb8a1b0dd860ea3219367
Pushpenderrathore <Pushpendersingh046@gmail.com>
2025-07-21 22:37:51 +0530

    modified and version added

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo cp install/failwipe /etc/initcpio/install
 sudo cp install/failwipe_encrypt /etc/initcpio/install/
 sudo chmod +x /etc/initcpio/hooks/failwipe*
 sudo chmod +x /etc/initcpio/install/failwipe*
+
+echo "!!! ⚠️ Warning: Do not forget your password. Entering the wrong password more than five times may trigger a destructive action and result in permanent data loss !!!"
