700cc27a6a270228686fa343f21eac2fd85b4767
Pushpenderrathore <Pushpendersingh046@gmail.com>
2025-07-22 14:39:19 +0530

    added sudo mkinitcpio -P

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ sudo cp install/failwipe_encrypt /etc/initcpio/install/
 sudo chmod +x /etc/initcpio/hooks/failwipe*
 sudo chmod +x /etc/initcpio/install/failwipe*
 
+sudo mkinitcpio -P
+
 echo "!!! ⚠️ Warning: Do not forget your password. Entering the wrong password more than five times may trigger a destructive action and result in permanent data loss !!!"
